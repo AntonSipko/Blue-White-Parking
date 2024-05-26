7d09f4a0b0c78d945946968f1d84a49e4e25f8ab
Anton Sipko <sipcoanton@gmail.com>
2024-05-26 21:25:42 +0300

    Starting Project

---
+++ blue-white-parking/src/main/java/telran/BlueWhiteParkingApplication.java
@@ -0,0 +1,13 @@
+package telran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueWhiteParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueWhiteParkingApplication.class, args);
+	}
+
+}
