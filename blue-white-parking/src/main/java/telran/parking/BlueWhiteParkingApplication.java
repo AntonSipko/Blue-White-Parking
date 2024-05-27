508d6c096ba1c13bf8e14ee96f65aeb79f5f741e
Anton Sipko <sipcoanton@gmail.com>
2024-05-27 21:04:06 +0300

    Camera immitator was created

--- blue-white-parking/src/main/java/telran/parking/BlueWhiteParkingApplication.java
+++ blue-white-parking/src/main/java/telran/parking/BlueWhiteParkingApplication.java
@@ -1,9 +1,11 @@
-package telran;
+package telran.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BlueWhiteParkingApplication {
 
 	public static void main(String[] args) {
